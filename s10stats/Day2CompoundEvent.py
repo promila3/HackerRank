faa17363bac7cc29ba8fa6d216fb5bbfc7dfeb21
promila3 <promila3@gmail.com>
2017-02-08 14:07:27 -0800

    Create Day2CompoundEvent.py
    There are  urns labeled , , and . 
    
    Urn  X contains  red balls and  black balls.
    Urn  Y contains  red balls and  black balls.
    Urn  Z contains  red balls and  black balls. 
    
    One ball is drawn from each of the  urns. What is the probability that, of the  balls drawn,  2 are red and 1  is black?

---
+++ s10stats/Day2CompoundEvent.py
@@ -0,0 +1,12 @@
+prx = (4/7.0)
+pbx = (3/7.0)
+pry = (5/9.0)
+pby = (4/9.0)
+pz = (1/2.0)
+
+prrb = prx * pry * pz
+prbr = prx * pby * pz
+pbrr = pbx * pry * pz
+
+print prrb + prbr + pbrr
+print 17/42.0
